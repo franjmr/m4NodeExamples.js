ca1a70128cdba25fc2db31a0c8dd0d4ef6a8aa1a
franciscojmr <franciscojmr@meta4.com>
2020-01-28 17:05:17 +0100

    Add Examples

---
+++ __src__/executeRequest.ts
@@ -0,0 +1,25 @@
+import { M4ApiNodejs } from "@franjmr/m4-node-api";
+
+async function example(){
+    const server = "http://franciscocaw10.meta4.com:5020";
+    const user = "ORLIEMOBILE";
+    const pass = "RUN";
+
+    const m4apinode = await M4ApiNodejs(server,user,pass);
+
+    await m4apinode.logonPromise();
+
+    await m4apinode.loadMetadataPromise(['PSCO_WDG_MY_TASKS']);
+    
+    const request = await m4apinode.executeMethodPromise("PSCO_WDG_MY_TASKS", "PSCO_WDG_MY_TASKS", "PLCO_LOAD", [null]);
+    const objRequest = request.getObject();
+    if( objRequest ){
+        const nodeRequest = objRequest.getNode("PSCO_WDG_FUNC_PROCESSES");
+        console.log("Method executed ok! Number of records is: " + nodeRequest.count());
+    }
+
+    await m4apinode.logoutPromise();
+    console.log("All done")
+}
+
+example();
---
+++ __src__/logon.ts
@@ -0,0 +1,16 @@
+import { M4ApiNodejs } from "@franjmr/m4-node-api";
+
+async function example(){
+    const server = "http://franciscocaw10.meta4.com:5020";
+    const user = "ORLIEMOBILE";
+    const pass = "RUN";
+
+    const m4apinode = await M4ApiNodejs(server,user,pass);
+
+    await m4apinode.logonPromise();
+    await m4apinode.logoutPromise();
+    
+    console.log("All done")
+}
+
+example();
