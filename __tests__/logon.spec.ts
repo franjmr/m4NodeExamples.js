ec84cd3e0c3ac8a85dfb39540797f786e1cfcd21
franciscojmr <franciscojmr@meta4.com>
2020-02-10 13:06:23 +0100

    Add jest-environment

--- __tests__/logon.spec.ts
+++ __tests__/logon.spec.ts
@@ -2,18 +2,25 @@
 import { M4ApiNodejs } from "@franjmr/m4-node-api";
 import { M4ApiNode } from "@franjmr/m4-node-api/dist/m4apiNode";
 
-describe("JSONFileUtils Sync suite", () => {
+describe("M4ApiNode.js suite", () => {
 
     let m4ApiNodejs: M4ApiNode ;
 
-    beforeEach(async ()=>{
-        const server = "http://franciscocaw10.meta4.com:5020";
+    /**
+     * @jest-environment jsdom
+     */
+    beforeEach(async (done)=>{
+        const server = "http://arya.meta4.com:5020";
         const user = "ORLIEMOBILE";
         const pass = "RUN";
         m4ApiNodejs = await M4ApiNodejs(server,user,pass);
+        done();
     });
 
+    /**
+     * @jest-environment jsdom
+     */
     test("should generate an api url to connect", async () => {
-        expect(m4ApiNodejs.apiUrl).toBeTruthy();
+        expect(m4ApiNodejs.getApiUrl()).toBeTruthy();
     });
 });
