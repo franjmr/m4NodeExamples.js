fa4e57575e68b771e2afc8a218cb38aa693b3c22
franciscojmr <franciscojmr@meta4.com>
2020-02-12 17:16:37 +0100

    Add jasmine-spec-reporter

--- __tests__/logon.success.mock.spec.ts
+++ __tests__/logon.success.mock.spec.ts
@@ -1,15 +1,16 @@
 import { M4ApiNodejs } from "@franjmr/m4-node-api";
 import { M4ApiNode } from "@franjmr/m4-node-api/dist/m4apiNode";
 
-describe("Logon suite", () => {
+describe("Logon Mock suite", () => {
 
     let m4ApiNodejs: M4ApiNode ;
 
     beforeAll(async ()=>{
         const server = "http://arya.meta4.com:5020";
-        const user = "ORLIEMOBILE";
-        const pass = "RUN";
+        const user = "LUISARAGONES";
+        const pass = "YGANAR_YGANAR_YGANAR_YGANAR";
         m4ApiNodejs = await M4ApiNodejs(server,user,pass);
+        spyOn(m4ApiNodejs,"logonPromise").and.returnValues(Promise.resolve("TengoUnAmigoJaponesQueEsSexadorDePollos"));
     });
 
     beforeAll(async() => {
@@ -18,16 +19,9 @@ describe("Logon suite", () => {
         }
     })
 
-    it("should generate an api url", async () => {
-        expect(m4ApiNodejs.getApiUrl()).toBeTruthy();
-    });
-
-    it("should use node api url", async () => {
-        expect(m4ApiNodejs.getApiUrl()).toContain("node");
-    });
-
     it("should do logon", async () => {
+        const logonToken = "TengoUnAmigoJaponesQueEsSexadorDePollos";
         const token = await m4ApiNodejs.logonPromise()
-        expect(token).toBeTruthy();
+        expect(token).toEqual(logonToken);
     });
 });
