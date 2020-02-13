7cf0b04179ba86e152a8792f43cf02e87ba0c29b
franciscojmr <franciscojmr@meta4.com>
2020-02-13 10:11:07 +0100

    Add mock logon M4logonResult

--- __tests__/logon.success.mock.spec.ts
+++ __tests__/logon.success.mock.spec.ts
@@ -1,16 +1,43 @@
 import { M4ApiNodejs } from "@franjmr/m4-node-api";
 import { M4ApiNode } from "@franjmr/m4-node-api/dist/m4apiNode";
+import { M4LogonResult } from "@franjmr/m4-node-api/dist/m4Interfaces/M4LogonResult";
 
 describe("Logon Mock suite", () => {
 
     let m4ApiNodejs: M4ApiNode ;
+    const logonToken: string = "TengoUnAmigoJaponesQueEsSexadorDePollos";
+
+    class MockM4LogonResult implements M4LogonResult {
+        getToken(): string {
+            return logonToken;
+        }
+        getLogonStatus(): number {
+            throw new Error("Method not implemented.");
+        }
+        getLogonStatusAsString(): string {
+            throw new Error("Method not implemented.");
+        }
+        getRoleId(): string {
+            throw new Error("Method not implemented.");
+        }
+        getOrganizationId(): string {
+            throw new Error("Method not implemented.");
+        }
+        getRoles(): any[] {
+            throw new Error("Method not implemented.");
+        }
+        isAuthenticated() {
+            return 'Mocked';
+        }
+    }
+    const mockM4LogonResult = new MockM4LogonResult();
 
     beforeAll(async ()=>{
         const server = "http://arya.meta4.com:5020";
         const user = "LUISARAGONES";
         const pass = "YGANAR_YGANAR_YGANAR_YGANAR";
         m4ApiNodejs = await M4ApiNodejs(server,user,pass);
-        spyOn(m4ApiNodejs,"logonPromise").and.returnValues(Promise.resolve("TengoUnAmigoJaponesQueEsSexadorDePollos"));
+        spyOn(m4ApiNodejs,"logonPromise").and.returnValues(Promise.resolve(mockM4LogonResult));
     });
 
     beforeAll(async() => {
@@ -20,8 +47,7 @@ describe("Logon Mock suite", () => {
     })
 
     it("should do logon", async () => {
-        const logonToken = "TengoUnAmigoJaponesQueEsSexadorDePollos";
-        const token = await m4ApiNodejs.logonPromise()
-        expect(token).toEqual(logonToken);
+        const m4LogonResult = await m4ApiNodejs.logonPromise()
+        expect(m4LogonResult.getToken()).toEqual(logonToken);
     });
 });
