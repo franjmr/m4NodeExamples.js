91ea27e64c1ded290c3720d162558861016cf92f
franciscojmr <franciscojmr@meta4.com>
2020-03-10 16:11:27 +0100

    Fix m4object name

--- __src__/createXmlMetadataFromServer.ts
+++ __src__/createXmlMetadataFromServer.ts
@@ -2,7 +2,7 @@ import { M4TestUtils } from "@automation/m4nodejs";
 
 async function example(){
     const m4TestUtils = new M4TestUtils();
-    await m4TestUtils.createXmlMetadataFile("http://jonsnow.meta4.com:13020", "JCM_ESS", "123", "SRCO_PA_WZ_FAMILY","C:/Temp");
+    await m4TestUtils.createXmlMetadataFile("http://jonsnow.meta4.com:13020", "JCM_ESS", "123", "PSCO_GTA_EDIT_CLOCK","C:/Temp");
 }
 
 example();
