98c1b768e1b3da91ecd19b8d207e8090b0d2656a
franciscojmr <franciscojmr@meta4.com>
2020-03-10 13:27:46 +0100

    Update ID_M4OBJECT

--- __src__/createXmlMetadataFromServer.ts
+++ __src__/createXmlMetadataFromServer.ts
@@ -2,7 +2,7 @@ import { M4TestUtils } from "@automation/m4nodejs";
 
 async function example(){
     const m4TestUtils = new M4TestUtils();
-    await m4TestUtils.createXmlMetadataFile("http://jonsnow.meta4.com:13020", "JCM_ESS", "123", "PLCO_LOAD_ALL_PERSONAL_INFO","C:/Temp");
+    await m4TestUtils.createXmlMetadataFile("http://jonsnow.meta4.com:13020", "JCM_ESS", "123", "SRCO_PA_WZ_FAMILY","C:/Temp");
 }
 
 example();
